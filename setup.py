69fa0d0dc7985620e574f67399c3b7644f11a611
Anandan B S <anandanklnce@gmail.com>
2025-05-26 12:32:05 +0530

    Update setup.py - version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="simplediskdb",
-    version="0.1.0",
+    version="0.2.0",
     author="Anandan B S",
     author_email="anandanklnce@gmail.com",
     description="A MongoDB-style disk-based database implementation",
